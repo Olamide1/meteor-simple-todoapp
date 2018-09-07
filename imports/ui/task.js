5b2a03f8275004ad4e585d8bd908f84aa51c01f0
obakomolafe <obakomolafe@royalniger.com>
2018-09-07 03:47:35 +0000

    Import Task component from the body

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -4,7 +4,7 @@ import {Tasks} from '../api/tasks.js';
 
 import './task.html';
 
-Template.task.events({
+Template.tasks.events({
     'click .toggle-checked'() {
         // Set the checked property to the opposite of its current value
         Tasks.update(this._id, {
